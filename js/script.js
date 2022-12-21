a2b1da6da489a7a51f379e1f9d578a1b15f77b41
PinoMacri <pinonegozio1@outlook.it>
2022-12-21 15:32:17 +0100

    first commit

---
+++ js/script.js
@@ -0,0 +1,23 @@
+/*
+//*MILESTONE 0:
+Creare l’array di oggetti con le informazioni fornite.
+//*MILESTONE 1:
+Stampare su console, per ogni membro del team, le informazioni di nome, ruolo e la stringa della foto
+//*MILESTONE 2:
+Stampare le stesse informazioni su DOM sottoforma di stringhe
+BONUS 1:
+Trasformare la stringa foto in una immagine effettiva
+BONUS 2:
+Organizzare i singoli membri in card/schede e rendere la pagina gradevole (potete usare lo screen in allegato come spunto
+*/
+const teamMember = [
+    {
+        Founder: ["Wayne Barnett", "wayne-barnett-founder-ceo.jpg"],
+        ChiefEditor: ["Angela Carrol", "angela-caroll-chief-editor.jpg"],
+        OfficeManager: ["Walter Gordon", "walter-gordon-office-manager.jpg"],
+        SocialMediaManager: ["Angela Lopez", "angela-lopez-social-media-manager.jpg"],
+        Developer: ["Scott Estrada", "scott-estrada-developer.jpg"],
+        GraphicDesigner: ["Barbara Ramos", "barbara-ramos-graphic-designer.jpg"],
+    
+    }
+]
